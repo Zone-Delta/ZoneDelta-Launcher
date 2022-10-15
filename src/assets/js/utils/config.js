ab07e4cc0076987a0ff4a92e744dcb52b1d5f777
Ambre <maxeae.gfx@gmail.com>
2022-10-15 18:46:01 +0200

    Add RSS news azuriom

--- src/assets/js/utils/config.js
+++ src/assets/js/utils/config.js
@@ -1,39 +1,48 @@
-/**
- * @author Luuxis
- * @license CC-BY-NC 4.0 - https://creativecommons.org/licenses/by-nc/4.0/
- */
 
-const pkg = require('../package.json');
-const fetch = require("node-fetch")
-let url = pkg.user ? `${pkg.url}/${pkg.user}` : pkg.url
 
-let config = `${url}/launcher/config-launcher/config.json`;
-let news = `${url}/launcher/news-launcher/news.json`;
-
-class Config {
-    GetConfig() {
-        return new Promise((resolve, reject) => {
-            fetch(config).then(config => {
-                return resolve(config.json());
-            }).catch(error => {
-                return reject(error);
-            })
-        })
-    }
-
-    async GetNews() {
-        let rss = await fetch(news);
-        if (rss.status === 200) {
-            try {
-                let news = await rss.json();
-                return news;
-            } catch (error) {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }
-}
-
-export default new Config;
+ const pkg = require('../package.json');
+ const fetch = require("node-fetch")
+ const convert = require("xml-js")
+ let url = pkg.user ? `${pkg.url}/${pkg.user}` : pkg.url
+ 
+ let config = `${url}/launcher/config-launcher/config.json`;
+ let news = `https://zone-delta.fr/api/rss`;
+ 
+ class Config {
+     GetConfig() {
+         return new Promise((resolve, reject) => {
+             fetch(config).then(config => {
+                 return resolve(config.json());
+             }).catch(error => {
+                 return reject(error);
+             })
+         })
+     }
+ 
+     async GetNews() {
+         let rss = await fetch(news).then(res => res.text());
+         let rssparse = JSON.parse(convert.xml2json(rss, { compact: true }));
+         let data = [];
+         if (rssparse.rss.channel.item.length) {
+             for (let i of rssparse.rss.channel.item) {
+                 let item = {}
+                 item.title = i.title._text;
+                 item.content = i['content:encoded']._text;
+                 item.author = i['dc:creator']._text;
+                 item.publish_date = i.pubDate._text;
+                 data.push(item);
+             }
+         } else {
+             let item = {}
+             item.title = rssparse.rss.channel.item.title._text;
+             item.content = rssparse.rss.channel.item['content:encoded']._text;
+             item.author = rssparse.rss.channel.item['dc:creator']._text;
+             item.publish_date = rssparse.rss.channel.item.pubDate._text;
+             data.push(item);
+ 
+         }
+         return data;
+     }
+ }
+ 
+ export default new Config;
